5b310bfb0adfd2f7226ef958302ac11d1f94815d
victorkudensik <70425083+victorkudensik@users.noreply.github.com>
2023-12-05 14:56:44 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Valera
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/valera-free-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
